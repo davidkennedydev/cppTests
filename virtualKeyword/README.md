47654bfc1916a1e75e639babb87c00e22778cfd5
David Kennedy S. A <daviduser.guj@gmail.com>
2017-11-24 18:01:45 -0200

    Improve documentation, with warnings about virtual destructor

---
+++ virtualKeyword/README.md
@@ -0,0 +1,7 @@
+#Virtual keyword uses (Polymophism)
+
+This example shows how virtual keyword can be used to create virtual fuctions that work at poliphic way at runtime.
+
+Functions with virtual gain polimorphism, that means subclasses implementation is ever called.
+Functions without virtual, are just class called, that means that the function member called is the function of the class of this instace definition, any cast can modify the called member function.
+Using raw pointers if your base class had some virtual member function a virtual destructor should be defined.
--- virtualKeyword/main.cpp
+++ virtualKeyword/main.cpp
@@ -1,29 +1,47 @@
+#include <memory>
 #include <iostream>
 using namespace std;
 
 class First {
-	public:
-	virtual void polimorphic() {
-		cout << "first: polimorphic call" << endl;
-	}
-	void overwrite() {
-		cout << "first: overwrite call" << endl;
-	}
+  public:
+  // XXX Every time that you have a class with vitual members,
+  // that should had a virtual destructor
+  virtual ~First() = default;
+
+  // virtual function
+  virtual void polimorphic() {
+    cout << "first: polimorphic call" << endl;
+  }
+
+  // common function
+  void overwrite() {
+    cout << "first: overwrite call" << endl;
+  }
 };
 
 class Second : public First {
-	public:
-	virtual void polimorphic() {
-		cout << "second: polimorphic call" << endl;
-	}
-	void overwrite() {
-		cout << "second: overwrite call" << endl;
-	}
+  public:
+  virtual void polimorphic() {
+    cout << "second: polimorphic call" << endl;
+  }
+  void overwrite() {
+    cout << "second: overwrite call" << endl;
+  }
 };
 
-main() {
-	First *obj = new Second();
-	obj->polimorphic();
-	obj->overwrite();
-	delete obj;
+int main() {
+  First * obj = new Second();
+
+  // this function is virtual so is called the derivated 
+  // at right of =
+  // class Second memberfunction definition
+  obj->polimorphic();
+
+  // this function isn't virtual so is called the instance type
+  // at right left of =
+  // class First memberfunction definition
+  obj->overwrite();
+
+  // XXX Delete instance of object
+  delete obj;
 }
